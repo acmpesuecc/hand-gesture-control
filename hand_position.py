18c691eea194a2ed02680c4db38c0c240468d552
Nishtha <nishthap981@gmail.com>
2023-10-08 22:03:07 +0530

    made 2 diff files

---
+++ hand_position.py
@@ -0,0 +1,58 @@
+import time
+import pyautogui
+
+def detect_hand_position(x, y, scrw, scrh):
+    # Define the regions for each key
+    leftarea = (0, 0, scrw // 2, scrh // 2)
+    uparea = (0, 0, scrw // 2, scrh // 2)
+    downarea = (scrw // 2, scrh // 2, scrw, scrh)
+    rightarea = (scrw // 2, scrh // 2, scrw, scrh)
+
+    if x < leftarea[2] and y > leftarea[1]:
+        return 'left'
+    elif x < uparea[2] and y < uparea[3]:
+        return 'up'
+    elif x > downarea[0] and y > downarea[1]:
+        return 'down'
+    elif x > rightarea[0] and y < rightarea[3]:
+        return 'right'
+    else:
+        return None
+
+def stimulate_keys():
+    scrw, scrh = pyautogui.size()
+
+    while True:
+        x, y = pyautogui.position()
+        hand_position = detect_hand_position(x, y, scrw, scrh)
+
+        if hand_position == 'left':
+            pyautogui.keyDown('left')
+            pyautogui.keyUp('right')
+            pyautogui.keyUp('up')
+            pyautogui.keyUp('down')
+        elif hand_position == 'up':
+            pyautogui.keyDown('up')
+            pyautogui.keyUp('down')
+            pyautogui.keyUp('left')
+            pyautogui.keyUp('right')
+        elif hand_position == 'down':
+            pyautogui.keyDown('down')
+            pyautogui.keyUp('up')
+            pyautogui.keyUp('left')
+            pyautogui.keyUp('right')
+        elif hand_position == 'right':
+            pyautogui.keyDown('right')
+            pyautogui.keyUp('left')
+            pyautogui.keyUp('up')
+            pyautogui.keyUp('down')
+        else:
+            pyautogui.keyUp('left')
+            pyautogui.keyUp('up')
+            pyautogui.keyUp('down')
+            pyautogui.keyUp('right')
+        
+        time.sleep(0.1) 
+
+if __name__ == "_main_":
+    stimulate_keys()
---
+++ main.py
@@ -0,0 +1,75 @@
+import numpy as np
+import mediapipe as mp
+import cv2
+import time
+import threading
+import hand_position
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+
+def hand_tracking():
+    with mp_hands.Hands(
+        model_complexity=1,
+        min_detection_confidence=0.4,
+        min_tracking_confidence=0.4) as hands:
+
+        while cap.isOpened():
+            success, image = cap.read()
+            h, w, c = image.shape
+            start = time.perf_counter()
+
+            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+            image.flags.writeable = False
+
+            results = hands.process(image)
+
+            image.flags.writeable = True
+
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    if hand_landmarks.landmark:
+                        mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                                                  mp_drawing_styles.get_default_hand_landmarks_style(),
+                                                  mp_drawing_styles.get_default_hand_connections_style())
+
+                        index_finger_tip = hand_landmarks.landmark[0]
+
+                        index_finger_tip_x = index_finger_tip.x * w
+                        index_finger_tip_y = index_finger_tip.y * h
+
+                        if index_finger_tip_x > w/2:
+                            cv2.putText(image, "Right", (500, 70),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 250))
+                        elif index_finger_tip_x < w/2:
+                            cv2.putText(image, "Left", (500, 70),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1.5, (250, 0))
+
+                cv2.line(image, (int(w/2), 0), (int(w/2), h), (0, 255, 0), 2)
+
+                end = time.perf_counter()
+                totalTime = end - start
+
+                fps = 1/totalTime
+
+                cv2.putText(image, f'FPS: {int(fps)}', (20, 70),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 2)
+                cv2.imshow('MediaPipe Hands', image)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+key_stimulation_thread = threading.Thread(target=hand_position.stimulate_keys)
+
+key_stimulation_thread.start()
+
+hand_tracking()
