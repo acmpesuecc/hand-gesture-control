b786bd7578a45ae862e955b81c9db8b48816aa01
CodeMaster1901 <abhirup1901@gmail.com>
2023-10-14 00:42:56 +0530

    issue#4 - spam key presses are avoided

--- hand_position.py
+++ hand_position.py
@@ -20,36 +20,39 @@ def detect_hand_position(x, y, scrw, scrh):
 
 def stimulate_keys():
     scrw, scrh = pyautogui.size()
+    prev_pos = None
 
     while True:
         x, y = pyautogui.position()
         hand_position = detect_hand_position(x, y, scrw, scrh)
-
-        if hand_position == 'left':
-            pyautogui.keyDown('left')
-            pyautogui.keyUp('right')
-            pyautogui.keyUp('up')
-            pyautogui.keyUp('down')
-        elif hand_position == 'up':
-            pyautogui.keyDown('up')
-            pyautogui.keyUp('down')
-            pyautogui.keyUp('left')
-            pyautogui.keyUp('right')
-        elif hand_position == 'down':
-            pyautogui.keyDown('down')
-            pyautogui.keyUp('up')
-            pyautogui.keyUp('left')
-            pyautogui.keyUp('right')
-        elif hand_position == 'right':
-            pyautogui.keyDown('right')
-            pyautogui.keyUp('left')
-            pyautogui.keyUp('up')
-            pyautogui.keyUp('down')
-        else:
-            pyautogui.keyUp('left')
-            pyautogui.keyUp('up')
-            pyautogui.keyUp('down')
-            pyautogui.keyUp('right')
+        if hand_position != prev_pos:
+            if hand_position == 'left':
+                pyautogui.keyDown('left')
+                pyautogui.keyUp('right')
+                pyautogui.keyUp('up')
+                pyautogui.keyUp('down')
+            elif hand_position == 'up':
+                pyautogui.keyDown('up')
+                pyautogui.keyUp('down')
+                pyautogui.keyUp('left')
+                pyautogui.keyUp('right')
+            elif hand_position == 'down':
+                pyautogui.keyDown('down')
+                pyautogui.keyUp('up')
+                pyautogui.keyUp('left')
+                pyautogui.keyUp('right')
+            elif hand_position == 'right':
+                pyautogui.keyDown('right')
+                pyautogui.keyUp('left')
+                pyautogui.keyUp('up')
+                pyautogui.keyUp('down')
+            else:
+                pyautogui.keyUp('left')
+                pyautogui.keyUp('up')
+                pyautogui.keyUp('down')
+                pyautogui.keyUp('right')
+            
+            prev_pos = hand_position
         
         time.sleep(0.1) 
 
